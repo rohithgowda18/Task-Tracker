a1a16d1b3d381218285a8f52aec36fb4927728ae
rohithgowda18 <rohithgowdak18@gmail.com>
2025-11-13 10:04:48 +0530

    enum and entity creation

---
+++ taskTrackerbackend/src/main/java/com/task/taskTracker/domain/entities/TaskPriority.java
@@ -0,0 +1,5 @@
+package com.task.taskTracker.domain.entities;
+
+public enum TaskPriority {
+    HIGH, MEDIUM, LOW
+}
---
+++ taskTrackerbackend/src/main/java/com/task/taskTracker/domain/entities/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.task.taskTracker.domain.entities;
+
+public enum TaskStatus {
+    OPEN, CLOSED
+}
